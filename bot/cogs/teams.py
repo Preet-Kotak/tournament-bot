c1a6761e95686f38a11cebabd5238b90be35cdb6
Preet_kotak <preetkotaktesting@gmail.com>
2026-06-07 13:41:06 +0530

    team channelk permission

--- bot/cogs/teams.py
+++ bot/cogs/teams.py
@@ -223,11 +223,21 @@ class Teams(commands.Cog):
                 
                 overwrites = {
                     guild.default_role: discord.PermissionOverwrite(read_messages=False),
-                    guild.me: discord.PermissionOverwrite(read_messages=True)
+                    guild.me: discord.PermissionOverwrite(read_messages=True,
+                                                          send_messages=True,
+                                                          read_message_history=True,
+                                                          add_reactions=True,
+                                                          attach_files=True,
+                                                          use_application_commands=True)
                 }
                 
                 if team_role:
-                    overwrites[team_role] = discord.PermissionOverwrite(read_messages=True)
+                    overwrites[team_role] = discord.PermissionOverwrite(read_messages=True,
+                                                          send_messages=True,
+                                                          read_message_history=True,
+                                                          add_reactions=True,
+                                                          attach_files=True,
+                                                          use_application_commands=True)
                     
                 channel = await guild.create_text_channel(
                     name=f"{team_name.lower().replace(' ', '-')}",
